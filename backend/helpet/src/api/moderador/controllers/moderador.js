2098dad96a2ca5b2199150853cdea5498615d953
Alef Porto <falejoaoalef@gmail.com>
2023-04-23 20:50:07 -0300

    backend

---
+++ backend/helpet/src/api/moderador/controllers/moderador.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * moderador controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::moderador.moderador');
